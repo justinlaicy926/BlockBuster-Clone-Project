0ae958b9d8eac362f0566fa077dcc75a5fda67c7
justinlaicy926 <justinlaicy926@gmail.com>
2022-09-15 15:29:36 -0700

    prepared for deployment

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ require('./startup/config')();
 
 require('./startup/validation')();
 
+require('./startup/prod')(app);
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
